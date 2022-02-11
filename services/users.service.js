d97d9bd8d9a16f5e83939ab176e0570c9a9b309d
Bruno Pinho <brunopinho@outlook.com>
2022-02-11 11:11:32 -0300

    feito requisito 1

--- services/users.service.js
+++ services/users.service.js
@@ -1,18 +1,34 @@
+require('dotenv').config();
+
 const Joi = require('joi');
+const JWT = require('jsonwebtoken');
 const errorHandling = require('../utils/errorHandling');
+
+const { User } = require('../models');
 
 const newUserSchema = Joi.object({
   displayName: Joi.string().min(8),
   email: Joi.string().email().required(),
   password: Joi.string().length(6).required(),
+  image: Joi.string(),
 });
 
 const createNewUser = async (displayName, email, password, image) => {
-  const { error } = newUserSchema.validate({ displayName, email, password });
+  const { error } = newUserSchema.validate({ displayName, email, password, image });
 
   if (error) throw errorHandling(400, error.message);
+  
+  const emailAlreadyExists = await User.findOne({ where: { email } });
 
-  return { message: 'xablau' };
+  if (emailAlreadyExists) throw errorHandling(409, 'User already registered');
+
+  const newUser = await User.create({ displayName, email, password, image });
+
+  const { id } = newUser;
+
+  const token = JWT.sign({ id, email }, process.env.JWT_SECRET);
+
+  return { token };
 };
 
 module.exports = {
